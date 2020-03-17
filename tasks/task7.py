88c71e335c974a13840feb30554b7459b9a8c61f
ilya <illyakobylatskyi@gmail.com>
2020-03-17 15:13:05 +0200

    2nd commit

--- tasks/task7.py
+++ tasks/task7.py
@@ -6,7 +6,7 @@ def count_special_chars(database_file):
         "=": 0,
         "-": 0,
         "|": 0,
-        "other_chars": 0
+        "alpha": 0
     }
     for char in database_file:
         if char == "=":
@@ -16,10 +16,23 @@ def count_special_chars(database_file):
         elif char == "|":
             special_chars["|"] += 1
         else:
-            special_chars["other_chars"] += 1
+            special_chars["alpha"] += 1
 
     return special_chars
 
 print(count_special_chars(database_file))
+
+#database_list = [{sales} = "snum": 1001, {customers} = "cnum": 2001, {orders} = "onum": 3001]
+
+# либо так
+# database_list = [{"snum": 1001}, {"cnum": 2001}, {"onum": 3001}]
+# еще можно вот так
+# {"sales": {"snum": 1001}, "customers": {"cnum": 2001}, "orders": {"onum": 3001}}
+
+res = count_special_chars("====================  sales  ================")
+if res["="] > 10:
+    database_file.strip("=")
+    database_file.strip()
+print(res)
 
 test_db_file.close()
